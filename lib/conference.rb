c392ddf05ccada2c3b94ff389ff54ae5afffd24c
chaduvulaprasanth <chaduvulaprasanth1998@gmail.com>
2020-08-03 22:29:50 +0530

    created afternoon sessions

--- lib/conference.rb
+++ lib/conference.rb
@@ -18,5 +18,6 @@ class Conference
     @session = Session.new(talks)
     track = Track.new(name)
     track_morning_sessions = session.morning_sessions(track)
+    track_afternoon_sessions = session.afternoon_sessions(track)
   end
 end
