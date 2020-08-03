bdd394c98e97a6b4a80c2f3d386a6798d5f26c8e
chaduvulaprasanth <chaduvulaprasanth1998@gmail.com>
2020-08-03 22:33:54 +0530

    added sessions to track

--- lib/session.rb
+++ lib/session.rb
@@ -14,10 +14,30 @@ class Session
   def morning_sessions(track)
     @start_time = Time.local(year, month, day, 9)
     @end_time = Time.local(year, month, day, 12)
+    common_job(track, @start_time, @end_time)
   end
 
   def afternoon_sessions(track)
     @start_time = Time.local(year, month, day, 13)
     @end_time = Time.local(year, month, day, 17)
+    common_job(track, @start_time, @end_time)
+  end
+
+  def common_job(track, start_time, end_time)
+    @available_minutes = (end_time.to_i - start_time.to_i) / 60
+    @talk_start_time =  start_time
+    @track = track
+    add_to_track
+  end
+
+  def add_to_track
+    talks.each do |talk, time|
+      if time <= @available_minutes
+        @track.add_sessions("#{@talk_start_time.strftime("%I:%M %p")} #{talk}") 
+        @available_minutes -= time
+        @talk_start_time += (time * 60) 
+        @talks.delete(talk)
+      end
+    end
   end
 end
